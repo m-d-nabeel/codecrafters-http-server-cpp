7dc375e5a4551549cda26b0de6a34f0ef9ed969f
m-d-nabeel <asharnabeel137@gmail.com>
2024-07-13 01:51:43 +0530

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -7,6 +7,64 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+struct Headers {
+  std::string host;
+  std::string user_agent;
+  std::string accept;
+
+  std::string to_string() {
+    return "Host: " + host + "\r\n" + "User-Agent: " + user_agent + "\r\n" + "Accept: " + accept +
+        "\r\n";
+  }
+
+  std::string Default() {
+    struct Headers headers;
+    headers.host       = "Host: localhost:4221";
+    headers.user_agent = "User-Agent: Mozilla/5.0";
+    headers.accept     = "Accept: text/html";
+
+    return std::move(headers.to_string());
+  }
+};
+
+struct Response {
+  std::string method;
+  std::string path;
+  std::string version;
+  Headers headers;
+  std::string body;
+
+  std::string to_string() {
+    return method + " " + path + " " + version + "\r\n" + headers.to_string() + "\r\n" + body;
+  }
+
+  std::string Default() {
+    struct Response response;
+    response.method             = "HTTP/1.1";
+    response.path               = "200";
+    response.version            = "OK";
+    response.headers.host       = "Host: localhost:4221";
+    response.headers.user_agent = "User-Agent: curl/7.88.1";
+    response.headers.accept     = "Accept: */*";
+    response.body               = "";
+
+    return std::move(response.to_string());
+  }
+
+  std::string NotFound() {
+    struct Response response;
+    response.method             = "HTTP/1.1";
+    response.path               = "404";
+    response.version            = "Not Found";
+    response.headers.host       = "Host: localhost:4221";
+    response.headers.user_agent = "User-Agent: curl/7.88.1";
+    response.headers.accept     = "Accept: */*";
+    response.body               = "404 Not Found";
+
+    return std::move(response.to_string());
+  }
+};
+
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
@@ -54,16 +112,39 @@ int main(int argc, char **argv) {
   int client = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
   std::cout << "Client connected\n";
 
-  // std::string status_line   = "HTTP/1.1 200 OK\n";
-  // std::string headers       = "";
-  // std::string response_body = "";
+  char buffer[1024] = {0};
+  int n             = recv(client, buffer, 1024, 0);
 
-  // std::string response_str = status_line + "\r\n" + headers + "\r\n" + response_body;
-  std::string response_str = "HTTP/1.1 200 OK\r\n\r\n";
+  if (n == -1) {
+    std::cerr << "Failed to receive data\n";
+    return 1;
+  }
 
-  send(client, response_str.c_str(), response_str.length(), 0);
+  std::cout << "Received " << n << " bytes\n";
 
-  std::cout << "Response sent\n";
+  std::string request(buffer);
+  std::cout << request << std::endl;
+
+  size_t pos               = request.find("\r\n");
+  std::string request_line = request.substr(0, pos);
+  request.erase(0, pos + 2);
+
+  pos                = request_line.find(" ");
+  std::string method = request_line.substr(0, pos);
+  request_line.erase(0, pos + 1);
+
+  pos              = request_line.find(" ");
+  std::string path = request_line.substr(0, pos);
+  request_line.erase(0, pos + 1);
+
+  if (path == "/" || path == "") {
+    const std::string response = Response().Default();
+    send(client, response.c_str(), response.length(), 0);
+  } else {
+    const std::string response = Response().NotFound();
+    send(client, response.c_str(), response.length(), 0);
+  }
+
   close(server_fd);
 
   return 0;
