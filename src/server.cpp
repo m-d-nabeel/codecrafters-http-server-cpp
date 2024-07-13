68ef1d009ac64f2769f602a89a090cf425fedd72
m-d-nabeel <asharnabeel137@gmail.com>
2024-07-14 04:10:23 +0530

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -367,11 +367,36 @@ struct Request parse_request(const std::string &request_str) {
 }
 
 std::string compress_string(const std::string &str) {
-  uLongf dest_len = compressBound(str.length());
-  Bytef *dest     = new Bytef[dest_len];
-  compress(dest, &dest_len, (const Bytef *)str.c_str(), str.length());
+  z_stream zs;
+  memset(&zs, 0, sizeof(zs));
 
-  std::string compressed_str((char *)dest, dest_len);
-  delete[] dest;
-  return compressed_str;
+  if (deflateInit2(&zs, Z_BEST_COMPRESSION, Z_DEFLATED, 15 + 16, 8, Z_DEFAULT_STRATEGY) != Z_OK) {
+    throw(std::runtime_error("deflateInit2 failed while compressing."));
+  }
+
+  zs.next_in  = (Bytef *)str.data();
+  zs.avail_in = str.size();
+
+  int ret;
+  char outbuffer[32768];
+  std::string outstring;
+
+  do {
+    zs.next_out  = reinterpret_cast<Bytef *>(outbuffer);
+    zs.avail_out = sizeof(outbuffer);
+
+    ret = deflate(&zs, Z_FINISH);
+
+    if (outstring.size() < zs.total_out) {
+      outstring.append(outbuffer, zs.total_out - outstring.size());
+    }
+  } while (ret == Z_OK);
+
+  deflateEnd(&zs);
+
+  if (ret != Z_STREAM_END) {
+    throw(std::runtime_error("Exception during zlib compression: " + std::to_string(ret)));
+  }
+
+  return outstring;
 }
